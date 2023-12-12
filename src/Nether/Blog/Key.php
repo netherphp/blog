65c3a7b77953c5e72baba196970d4241c9b036de
bob <bob@tech.live>
2023-12-12 21:03:59 +0000

    thusfar

--- src/Nether/Blog/Key.php
+++ src/Nether/Blog/Key.php
@@ -5,12 +5,13 @@ namespace Nether\Blog;
 class Key {
 
 	const
-	ConfEnable       = 'Nether.Blog.Enable',
-	ConfBlogsPerUser = 'Nether.Blog.BlogsPerUser',
-	ConfStorageKey   = 'Nether.Blog.StorageKey',
-	ConfBlogURL      = 'Nether.Blog.BlogURL',
-	ConfPostURL      = 'Nether.Blog.PostURL',
-	ConfBlogSiteTags = 'Nether.Blog.SiteTags';
+	ConfEnable        = 'Nether.Blog.Enable',
+	ConfBlogsPerUser  = 'Nether.Blog.BlogsPerUser',
+	ConfStorageKey    = 'Nether.Blog.StorageKey',
+	ConfBlogURL       = 'Nether.Blog.BlogURL',
+	ConfPostURL       = 'Nether.Blog.PostURL',
+	ConfBlogSiteTags  = 'Nether.Blog.SiteTags',
+	ConfBlogIndexArea = 'Nether.Blog.IndexArea';
 
 	const
 	AccessBlogCreate = 'Nether.Blog.Create';
